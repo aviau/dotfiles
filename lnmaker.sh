b8f4e450d93810cc03a07e061763608b7d5b282b
Alexandre Viau <alexandre@alexandreviau.net>
2014-05-26 00:33:26 -0400

    Updated lnmaker

--- lnmaker.sh
+++ lnmaker.sh
@@ -8,21 +8,19 @@ fi
 dir=$(readlink -f $1)
 
 # .tmux.conf
-mv ~/.tmux.conf ~/.tmux.conf.bak
 ln ${dir}/.tmux.conf ~/.tmux.conf --symbolic
 
 # .atom
-mv ~/.atom ~/.atom.bak
 ln ${dir}/.atom ~/.atom --symbolic
 
 # .bash_profile
-mv ~/.bash_profile ~/.bash_profile.bak
 ln ${dir}/.bash_profile ~/.bash_profile --symbolic
 
 # .bashrc
-mv ~/.bashrc ~/.bashrc.bak
 ln ${dir}/.bashrc ~/.bashrc --symbolic
 
 # .bin
-mv ~/bin ~/bin.bak
 ln ${dir}/bin ~/bin --symbolic
+
+# awesome
+ln ${dir}/.config/awesome ~/.config/awesome --symbolic
