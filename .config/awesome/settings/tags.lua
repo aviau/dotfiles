4ae16dc9dcf5b34c10e44c9fcc1f7e586177818d
Alexandre Viau <alexandre@alexandreviau.net>
2014-05-26 00:36:12 -0400

    New tags

--- .config/awesome/settings/tags.lua
+++ .config/awesome/settings/tags.lua
@@ -17,18 +17,13 @@ layouts = {
 
 
 tags = {
-   names = { 1, 2, 3, 4, 5, 6, 7, 8, 9 },  -- default
-   -- names = { "ƀ", "Ƅ", "Ɗ", "ƈ", "ƙ" }, -- blackburn/dremora
+   names = { "WEB", "TERMINAL", "FILES", "OTHER" },  -- default
+
    layout = {
      layouts[1],
      layouts[2],
      layouts[3],
      layouts[4],
-     layouts[5],
-     layouts[6],
-     layouts[7],
-     layouts[8],
-     layouts[9],
    }
 }
 
