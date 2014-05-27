5886285bd91a0aa1da8fc769db1c3f2f780fd83d
aviau <alexandre.viau@savoirfairelinux.com>
2014-05-27 12:14:42 -0400

    Added mv commands

--- lnmaker.sh
+++ lnmaker.sh
@@ -8,19 +8,25 @@ fi
 dir=$(readlink -f $1)
 
 # .tmux.conf
+mv ~/.tmux.conf ~/.tmux.conf.bak
 ln ${dir}/.tmux.conf ~/.tmux.conf --symbolic
 
 # .atom
+mv ~/.atom ~/.atom.bak
 ln ${dir}/.atom ~/.atom --symbolic
 
 # .bash_profile
+mv ~/.bash_profile ~/.bash_profile.bak
 ln ${dir}/.bash_profile ~/.bash_profile --symbolic
 
 # .bashrc
+mv ~/.bashrc ~/.bashrc.bak
 ln ${dir}/.bashrc ~/.bashrc --symbolic
 
 # .bin
+mv ~/bin ~/.bin.bak
 ln ${dir}/bin ~/bin --symbolic
 
 # awesome
+mv ~/.config/awesome ~/.config/awesome.bak
 ln ${dir}/.config/awesome ~/.config/awesome --symbolic
