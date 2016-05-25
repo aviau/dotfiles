a83c378ba88886dededaa9cf0f641def4a3920e8
aviau <alexandre@alexandreviau.net>
2016-05-25 13:30:14 -0400

    avoid ycm_extra_conf code duplication

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -49,7 +49,10 @@ flags = [
 #
 # Most projects will NOT need to set this to anything; you can just change the
 # 'flags' list of compilation flags. Notice that YCM itself uses that approach.
-compilation_database_folder = ''
+compilation_database_folder = os.environ.get(
+    'YCM_COMPILATION_DATABASE_FOLDER',
+    ''
+)
 
 if os.path.exists( compilation_database_folder ):
   database = ycm_core.CompilationDatabase( compilation_database_folder )
---
+++ ycm/ring-daemon/.ycm_extra_conf.py
@@ -0,0 +1,8 @@
+import imp
+import os
+
+os.environ['YCM_COMPILATION_DATABASE_FOLDER'] = '/home/reazem/git/dotfiles/ycm/ring-daemon'
+main_config_path = os.path.expanduser('~/.ycm_extra_conf.py')
+
+imp.load_source('main_config', main_config_path)
+from main_config import *
