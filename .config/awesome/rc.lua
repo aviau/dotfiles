03757687ded5dc963fcf46c131364425169407ce
aviau <alexandre@alexandreviau.net>
2017-01-31 12:18:42 -0500

    Awesome: disable non-empty tag browsing

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -213,10 +213,10 @@ globalkeys = awful.util.table.join(
               {description = "go back", group = "tag"}),
 
     -- Non-empty tag browsing
-    awful.key({ altkey }, "Left", function () lain.util.tag_view_nonempty(-1) end,
-              {description = "view  previous nonempty", group = "tag"}),
-    awful.key({ altkey }, "Right", function () lain.util.tag_view_nonempty(1) end,
-              {description = "view  previous nonempty", group = "tag"}),
+--    awful.key({ altkey }, "Left", function () lain.util.tag_view_nonempty(-1) end,
+--              {description = "view  previous nonempty", group = "tag"}),
+--    awful.key({ altkey }, "Right", function () lain.util.tag_view_nonempty(1) end,
+--              {description = "view  previous nonempty", group = "tag"}),
 
     -- Default client focus
     awful.key({ altkey,           }, "j",
