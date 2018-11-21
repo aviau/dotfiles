a523133b67fa786accfad7b77270274777c51e03
aviau <alexandre@alexandreviau.net>
2018-11-20 21:52:31 -0500

    awesome: let firefox spawn wherever

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -679,8 +679,8 @@ awful.rules.rules = {
       properties = { titlebars_enabled = false } },
 
     -- Set Firefox to always map on the first tag on screen 1.
-    { rule = { class = "Firefox" },
-      properties = { screen = 1, tag = awful.util.tagnames[1] } },
+--    { rule = { class = "Firefox" },
+--      properties = { screen = 1, tag = awful.util.tagnames[1] } },
 
     { rule = { class = "Gimp", role = "gimp-image-window" },
           properties = { maximized = true } },
