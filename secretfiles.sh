22ebc3797e9d4d51d4ae58506b1bb9f287a6d89f
aviau <alexandre@alexandreviau.net>
2021-01-01 22:34:40 -0500

    secretfiles.sh: add perkeep

--- secretfiles.sh
+++ secretfiles.sh
@@ -8,6 +8,13 @@ set -e
 
 pass cgm.alexandreviau.net_readonly > ~/.config/nightscout_readonly_key
 
+# mutt
 pass dotfiles/muttrc > ~/.muttrc
 
+# ssh
 pass dotfiles/ssh/config > ~/.ssh/config
+
+# perkeep
+mkdir -p ~/.config/perkeep
+pass dotfiles/perkeep/server-config.json > ~/.config/perkeep/server-config.json
+chmod 600 ~/.config/perkeep/server-config.json
