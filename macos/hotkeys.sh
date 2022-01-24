b7bcb5a2536691125feca768284664500de3588a
aviau <alexandre@alexandreviau.net>
2022-01-23 20:24:12 -0500

    macos: create WIP hotkeys script

---
+++ macos/hotkeys.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env zsh
+
+set -eo pipefail
+
+# Find hotkeys with: 'defaults find NSUserKeyEquivalents'
+
+echo "Restoring keyboard shortcuts..."
+
+defaults write com.google.Chrome NSUserKeyEquivalents ' {
+    Back = "^\U2190";
+    Forward = "^\U2192";
+}'
+
+# This fails :(
+defaults write com.apple.universalaccess "com.apple.custommenu.apps" -array-add "<string>com.googlecode.iterm2</string>"
+
+defaults write "com.googlecode.iterm2" NSUserKeyEquivalents '{
+    Copy = "@$c";
+    Paste = "@$v";
+}'
+
+#killall Finder
+#killall cfprefsd
