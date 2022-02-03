d5379c227b09bcefd4bb794eafe962afd35fec9e
aviau <alexandre@alexandreviau.net>
2022-02-03 13:25:45 -0500

    implement unnaturalscrollwheels in lua

--- macos/.hammerspoon/init.lua
+++ macos/.hammerspoon/init.lua
@@ -2,7 +2,21 @@
 -- Window Management shortcuts --
 ---------------------------------
 hs.hotkey.bind({"option"}, "F", function()
-  -- Toggle Fullscreen
-  local win = hs.window.frontmostWindow()
-  win:toggleFullScreen()
+    -- Toggle Fullscreen
+    local win = hs.window.frontmostWindow()
+    win:toggleFullScreen()
 end)
+
+------------------------------
+-- Discrete Mouse Scrolling --
+------------------------------
+local scrollWatcher = hs.eventtap.new({hs.eventtap.event.types.scrollWheel}, function(event)
+    -- Similar to unnaturalscrollwheels's discrete scrolling.
+    local verticalScroll = event:getProperty(hs.eventtap.event.properties.scrollWheelEventDeltaAxis1)
+    if verticalScroll > 0 then
+        event:setProperty(hs.eventtap.event.properties.scrollWheelEventDeltaAxis1, 3)
+    else
+        event:setProperty(hs.eventtap.event.properties.scrollWheelEventDeltaAxis1, -3)
+    end
+end)
+scrollWatcher:start()
