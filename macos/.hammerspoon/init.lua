40268d23c3d593909d2bd3aefa3dce3445218120
aviau <alexandre@alexandreviau.net>
2022-02-03 09:53:18 -0500

    hammerspoon: window management section

--- macos/.hammerspoon/init.lua
+++ macos/.hammerspoon/init.lua
@@ -1,4 +1,8 @@
+---------------------------------
+-- Window Management shortcuts --
+---------------------------------
 hs.hotkey.bind({"option"}, "F", function()
+  -- Toggle Fullscreen
   local win = hs.window.frontmostWindow()
   win:toggleFullScreen()
 end)
